92167379971441f7d8fc0e0e1248169ee0734d3c
Archana Sharma <archanakumari202068@gmail.com>
2023-09-03 20:31:27 +0530

    question fetch from mongodb

--- server/models/Questions.js
+++ server/models/Questions.js
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 const QuestionSchema= mongoose.Schema({
     questionTitle:{type:String,required:"Question must have a title"},
     questionBody:{type:String,required:"Question must have a body"},
-    questionTags:{type:String,required:"Question must have a tags"},
+    questionTags:{type:[String],required:"Question must have a tags"},
     noOfAnswers:{type:Number,default:0},
-    upVote:{type:[String],default:[]},
-    downVotes:{type:[String],default:[]},
+    upVotes: { type: [String], default: [] },
+    downVotes: { type: [String], default: [] },
     userPosted:{type:String,required:"Question must have an author"},
     userId:{type:String},
-    askedOn:{type:DataTransfer,default:Data.now},
+    askedOn:{type:Date,default:Date.now},
     answer:[{
         answerBody:String,
         userAnswered:String,
         UserId:String,
-        answeredOn:{type:Data,default:Data.now}
+        answeredOn:{type:Date,default:Date.now}
     }]
 })
 export default mongoose.model("Question",QuestionSchema)
--- server/routes/Questions.js
+++ server/routes/Questions.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import {AskQuestion} from '../controllers/Questions.js'
+import {getAllQuestions} from '../controllers/Questions.js'
+
 
 const router =express.Router()
 
 router.post('/Ask',AskQuestion)
+//send the get req to the database
+router.get('/get',getAllQuestions)
 export default router
